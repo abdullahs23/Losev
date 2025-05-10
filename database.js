e262c8283841454f0e22bcaa2d6281a95acd84f2
abdullahs23 <halil57631@gmail.com>
2025-05-10 17:05:25 +0300

    baglantı bilgileri gizlendi

--- database.js
+++ database.js
@@ -2,10 +2,17 @@ const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
 // Veritabanı bağlantısını oluştur
-const sequelize = new Sequelize('losev', 'root', process.env.DATABASE_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host:process.env.DB_HOST,
+        port:process.env.DB_PORT ||3306,
+        dialect:`mysql`,
+        logging:false
+    }
+);
 
 // Modelleri tanımla
 const userModel = require("./models/users")(sequelize, DataTypes);
